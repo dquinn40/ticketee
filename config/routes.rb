179d04bc31a39c36d2a94e86740dbfc15c612315
Dan <dquinn40@gmail.com>
2014-01-18 09:19:47 -0500

    Added user model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Ticketee::Application.routes.draw do
     resources :projects do
       resources :tickets
     end
+
+    namespace :api do
+      namespace :v1 do
+        resources :projects
+      end
+    end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
